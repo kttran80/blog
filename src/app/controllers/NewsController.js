05ca7408c476981ccad9e24941e6ac7430a08cf1
Kiet Tran <tranthekiet@yahoo.com>
2022-06-05 17:23:27 -0500

    organizing to MVC with routers

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,17 @@
+
+class NewsController{   
+   
+    // GET /news
+    index(req, res) {        
+        res.render('news');
+    }
+
+    show(req, res) {
+        res.send('NEWS DETAILS');
+    }
+
+
+}
+
+module.exports = new NewsController;
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //http logger
@@ -19,13 +22,9 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 console.log('PATH: ', path.join(__dirname, 'public'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
